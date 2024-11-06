cfb966eee88c9702a943febcf90f8751faf90aec
jesseswanton <42706542+jesseswanton@users.noreply.github.com>
2024-11-06 01:34:47 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <Footbar
       links={[
         <Link key={1} className="text-light fs-5" to="https://github.com/jesseswanton/">
-          My Github &nbsp; &nbsp; <img src="https://github.com/jesseswanton/reactportfolio/blob/main/assets/github.png" alt="Github logo" width="50" height="50" />
+          My Github &nbsp; &nbsp; <img src="https://raw.githubusercontent.com/jesseswanton/reactportfolio/refs/heads/main/assets/github.png" alt="Github logo" width="50" height="50" />
         </Link>,
       ]}
     />
